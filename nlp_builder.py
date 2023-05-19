592ad5cce597f8687d62a3e0eb102377b1dacc4d
Kanghyun Kim <kh11kim@kaist.ac.kr>
2023-05-19 18:29:07 +0900

    soft const

--- nlp_builder.py
+++ nlp_builder.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 from functools import partial
 
 class NLPBuilder:
@@ -8,6 +9,8 @@ class NLPBuilder:
         self.f = None
         self._g = [] # eq = 0.
         self._h = [] # ineq <= 0.
+        self._g_jac_sparsity = []
+        self._h_jac_sparsity = []
         self.lb = None
         self.ub = None
         self.g_dim = 0
@@ -16,6 +19,10 @@ class NLPBuilder:
     @property
     def const_dim(self):
         return self.g_dim + self.h_dim
+    
+    @property
+    def xrand(self):
+        return jnp.array(np.random.random(self.dim))
     
     def get_output_len(self, fn):
         out = fn(jnp.zeros(self.dim))
@@ -27,33 +34,51 @@ class NLPBuilder:
     def set_f(self, fn):
         self.f = fn
     
-    def add_eq_const(self, fn, val):
+    def jacobian_sparsity(self, fn, est):
+        if est:
+            jac_sparsity = jax.jacfwd(fn)(self.xrand) != 0.
+        else:
+            num_const = 1 if len(fn(self.xrand).shape) == 0 else fn(self.xrand).shape[0]
+            jac_shape = num_const, self.dim
+            jac_sparsity = jnp.ones(jac_shape).astype(bool)
+        return jac_sparsity
+
+    def add_eq_const(self, fn, val, est_sparsity=False):
         fn_new = lambda x: fn(x) - val
         self._g.append(fn_new)
         self.g_dim += self.get_output_len(fn_new)
+        self._g_jac_sparsity.append(self.jacobian_sparsity(fn_new, est_sparsity))
 
-    def add_ineq_const_lb(self, fn, lb):
+    def add_ineq_const_lb(self, fn, lb, est_sparsity=False):
         fn_new = lambda x: -fn(x) + lb
         self._h.append(fn_new)
         self.h_dim += self.get_output_len(fn_new)
+        self._h_jac_sparsity.append(self.jacobian_sparsity(fn_new, est_sparsity))
     
-    def add_ineq_const_ub(self, fn, ub):
+    def add_ineq_const_ub(self, fn, ub, est_sparsity=False):
         fn_new = lambda x: fn(x) - ub
         self._h.append(fn_new)
         self.h_dim += self.get_output_len(fn_new)
+        self._h_jac_sparsity.append(self.jacobian_sparsity(fn_new, est_sparsity))
 
-    def add_ineq_const_b(self, fn, lb, ub):
-        lb_fn = lambda x: -fn(x) + lb
-        ub_fn = lambda x: fn(x) - ub
-        self._h.append(lb_fn)
-        self._h.append(ub_fn)
-        self.h_dim += self.get_output_len(lb_fn) * 2
+    def add_ineq_const_b(self, fn, lb, ub, est_sparsity=False):
+        self.add_ineq_const_lb(fn, lb, est_sparsity)
+        self.add_ineq_const_ub(fn, ub, est_sparsity)
+        # lb_fn = lambda x: -fn(x) + lb
+        # ub_fn = lambda x: fn(x) - ub
+        # self._h.append(lb_fn)
+        # self._h.append(ub_fn)
+        # self.h_dim += self.get_output_len(lb_fn) * 2
 
     def set_state_bound(self, xlb, xub):
-        def state_bound_fn(x):
-            return jnp.hstack([-x + xlb, x - xub])
-        self._h.append(state_bound_fn)
-        self.h_dim += self.dim*2
+        # def state_bound_fn(x):
+        #     return jnp.hstack([-x + xlb, x - xub])
+        self.add_ineq_const_b(lambda x: x, xlb, xub, True)
+        # self._h.append(state_bound_fn)
+        # self.h_dim += self.dim*2
+        
+        # jac_sparsity = jax.jacfwd(state_bound_fn)(self.xrand) != 0.
+        # self._h_jac_sparsity.append(jac_sparsity)
 
     def get_g_fn(self):
         return lambda x: jnp.hstack([fn(x) for fn in self._g])
@@ -64,33 +89,41 @@ class NLPBuilder:
     def get_gh_fn(self):
         return lambda x: jnp.hstack([fn(x) for fn in self._g+self._h])
 
-    def get_lagrangian_fn(self):
-        gh = self.get_gh_fn()
-        return lambda x, lmbda: self.f(x) + gh(x) @ lmbda
+    def get_g_jac_sparsity(self):
+        return jnp.vstack(self._g_jac_sparsity)
     
-    def get_merit_fn(self):
-        f = self.f
-        g = self.get_g_fn()
-        h = self.get_h_fn()
-        def merit_fn(x, sigma=0.):
-            eq_norm = jnp.linalg.norm(g(x), 1)
-            ineq_norm = jnp.linalg.norm(jnp.clip(h(x), a_min=0.), 1)
-            return f(x) + sigma * (eq_norm + ineq_norm)
-        return merit_fn
+    def get_h_jac_sparsity(self):
+        return jnp.vstack(self._h_jac_sparsity)
     
-    def get_merit_direc_deriv_fn(self):
-        g = self.get_g_fn()
-        h = self.get_h_fn()
-        f_grad_fn = jax.grad(self.f)
-        eq_dim = self.g_dim
-        ineq_dim = self.h_dim
-        def merit_direc_deriv(x, direction, sigma):
-            direct_deriv = f_grad_fn(x) @ direction
-            if eq_dim != 0:
-                eq_1norm = jnp.linalg.norm(g(x), 1)
-            else:
-                eq_1norm = 0.
-            ineq_1norm = 0. if ineq_dim == 0 \
-                else jnp.linalg.norm(jnp.clip(h(x),a_min=0.), 1)
-            return direct_deriv - sigma * (eq_1norm + ineq_1norm)
-        return merit_direc_deriv
+    
+    
+    # def get_lagrangian_fn(self):
+    #     gh = self.get_gh_fn()
+    #     return lambda x, lmbda: self.f(x) + gh(x) @ lmbda
+    
+    # def get_merit_fn(self):
+    #     f = self.f
+    #     g = self.get_g_fn()
+    #     h = self.get_h_fn()
+    #     def merit_fn(x, sigma=0.):
+    #         eq_norm = jnp.linalg.norm(g(x), 1)
+    #         ineq_norm = jnp.linalg.norm(jnp.clip(h(x), a_min=0.), 1)
+    #         return f(x) + sigma * (eq_norm + ineq_norm)
+    #     return merit_fn
+    
+    # def get_merit_direc_deriv_fn(self):
+    #     g = self.get_g_fn()
+    #     h = self.get_h_fn()
+    #     f_grad_fn = jax.grad(self.f)
+    #     eq_dim = self.g_dim
+    #     ineq_dim = self.h_dim
+    #     def merit_direc_deriv(x, direction, sigma):
+    #         direct_deriv = f_grad_fn(x) @ direction
+    #         if eq_dim != 0:
+    #             eq_1norm = jnp.linalg.norm(g(x), 1)
+    #         else:
+    #             eq_1norm = 0.
+    #         ineq_1norm = 0. if ineq_dim == 0 \
+    #             else jnp.linalg.norm(jnp.clip(h(x),a_min=0.), 1)
+    #         return direct_deriv - sigma * (eq_1norm + ineq_1norm)
+    #     return merit_direc_deriv
--- nlp_solver.py
+++ nlp_solver.py
@@ -2,11 +2,12 @@ import numpy as np
 import jax
 from jax import Array
 import jax.numpy as jnp
-from typing import List, Dict, Callable
+from typing import List, Dict, Tuple, Callable
 import jax_dataclasses as jdc
 from nlp_builder import NLPBuilder
-from cvxopt import matrix, sparse, solvers
+from cvxopt import matrix, sparse, solvers, spmatrix
 from functools import partial
+from jax.experimental.sparse import BCOO as jsparse
 
 @jdc.pytree_dataclass
 class OptState:
@@ -22,16 +23,27 @@ class OptState:
     h_val: Array
     h_jac: Array
 
+@jdc.pytree_dataclass
+class History:
+    alpha: Array
+    direction: Array
+
 def value_and_jacrev(x, f):
     y, pullback = jax.vjp(f, x)
     basis = jnp.eye(y.size, dtype=y.dtype)
     jac = jax.vmap(pullback)(basis)[0]
     return y, jac
 
+def value_and_jacrev_sparse(x, f, row, col):
+    y, pullback = jax.vjp(f, x)
+    basis = jnp.eye(y.size, dtype=y.dtype)
+    jac = jax.vmap(pullback)(basis)[0]
+    return y, jac[row, col]
+
 def regularized_hess(P):
     def regularization(P):
         eigs, vecs = jnp.linalg.eigh(P)
-        delta = 1e-6
+        delta = 1e-5
         eigs_modified = jnp.where(eigs < delta, delta, eigs) #jnp.maximum(-eigs, delta)
         return vecs @ jnp.diag(eigs_modified) @ vecs.T
     def identity(P):
@@ -45,28 +57,122 @@ class SQPSolver:
     def __init__(
         self, 
         dim: int, 
-        const_dim: int,
-        eval_fn:Callable,
-        armijo_cond_fn: Callable,
+        eq_dim: int,
+        ineq_dim: int,
+        f_fn: Callable,
+        g_fn: Callable,
+        h_fn: Callable,
+        g_jac_sparsity: Array=None,
+        h_jac_sparsity: Array=None
     ):
         self.dim = dim
-        self.const_dim = const_dim
-        self.eval_fn = eval_fn
-        self.armijo_cond_fn = armijo_cond_fn
+        self.eq_dim = eq_dim
+        self.ineq_dim = ineq_dim
+        self.const_dim = eq_dim + ineq_dim
+        self.states = []
         self.history = []
 
-    def prebuild(self):
-        #???
-        x = jnp.zeros(self.dim)
-        m = jnp.zeros(self.const_dim)
-        sigma = 0.
-        state = self.eval_fn(x, m, sigma)
-        self.armijo_cond_fn(x, x, 0., 0., 0.)
-        self.max_viol(state)
-        self.max_grad(state)
-        self.backtrack(state, x)
-        self.update(state, 0., x, m)
+        self.f_fn = f_fn
+        self.g_fn = g_fn
+        self.h_fn = h_fn
+        self.lag_fn = lambda x, m: \
+            self.f_fn(x) + jnp.hstack([self.g_fn(x), self.h_fn(x)]) @ m
+        self.f_val_grad_fn = jax.value_and_grad(self.f_fn)
+        if g_jac_sparsity is None or h_jac_sparsity is None:
+            self.sparse = False
+            self.g_val_jac_fn = partial(value_and_jacrev, f=self.g_fn)
+            self.h_val_jac_fn = partial(value_and_jacrev, f=self.h_fn)
+        else:
+            self.sparse = True
+            self.g_jac_sparsity = g_jac_sparsity
+            self.h_jac_sparsity = h_jac_sparsity
+            self.g_jac_sparsity_rowcol = jnp.nonzero(g_jac_sparsity)
+            self.h_jac_sparsity_rowcol = jnp.nonzero(h_jac_sparsity)
+            self.g_val_jac_fn = partial(value_and_jacrev_sparse, 
+                       f=self.g_fn, 
+                       row=self.g_jac_sparsity_rowcol[0],
+                       col=self.g_jac_sparsity_rowcol[1])
+            self.h_val_jac_fn = partial(value_and_jacrev_sparse, 
+                       f=self.h_fn, 
+                       row=self.h_jac_sparsity_rowcol[0],
+                       col=self.h_jac_sparsity_rowcol[1])
+        
+    def prebuild(self, mode="exact"):
+        def eval_fn(x, m):
+            f_val, f_grad = self.f_val_grad_fn(x)
+            g_val, g_jac = self.g_val_jac_fn(x)
+            h_val, h_jac = self.h_val_jac_fn(x)
+            lag_grad = jax.grad(self.lag_fn)(x, m)
+            return f_val, f_grad, g_val, g_jac, h_val, h_jac, lag_grad
 
+        def eval_fn_exact(x, m, sigma, state_prev:OptState):
+            f_val, f_grad, g_val, g_jac, h_val, h_jac, lag_grad = eval_fn(x, m)
+            lag_hess = jax.hessian(self.lag_fn)(x, m)
+            lag_hess = regularized_hess(lag_hess)
+            return OptState(x, m, sigma, 
+                            lag_grad, lag_hess, f_val, f_grad,
+                            g_val, g_jac, h_val, h_jac)
+        
+        def eval_fn_BFGS(x, m, sigma, state_prev:OptState):
+            f_val, f_grad, g_val, g_jac, h_val, h_jac, lag_grad = eval_fn(x, m)
+            B_prev = state_prev.lag_hess
+            s = x - state_prev.x
+            y = lag_grad - state_prev.lag_grad
+            den1 = s @ B_prev @ s
+            den2 = jnp.inner(y, s)
+            # term1 = B_prev @ jnp.outer(s, s) @ B_prev / den1
+            # term2 = jnp.outer(y, y) / den2
+
+            # powell's trick (preserving positive-definiteness)
+            powell_cond = den2 >= 0.2 * den1
+            true_fn = lambda den1, den2: 1.
+            false_fn = lambda den1, den2: 0.8 * den1 / (den1 - den2)
+            theta = jax.lax.cond(powell_cond, true_fn, false_fn, den1, den2)
+            r = theta*y + (1 - theta) * B_prev@s
+            term1 = B_prev @ jnp.outer(s, s) @ B_prev / den1
+            term2 = jnp.outer(r,r)/jnp.inner(s, r)
+            lag_hess = B_prev - term1 + term2
+            return OptState(x, m, sigma, 
+                            lag_grad, lag_hess, f_val, f_grad,
+                            g_val, g_jac, h_val, h_jac)
+        
+        state_init_fn_exact = partial(eval_fn_exact, state_prev=None)
+        def state_init_fn_BFGS(x, m, sigma):
+            f_val, f_grad, g_val, g_jac, h_val, h_jac, lag_grad = eval_fn(x, m)
+            lag_hess = jnp.eye(x.shape[0])
+            return OptState(x, m, sigma, 
+                            lag_grad, lag_hess, f_val, f_grad,
+                            g_val, g_jac, h_val, h_jac)
+        
+        def merit_fn(x, sigma=0.):
+            eq_norm = jnp.linalg.norm(self.g_fn(x), 1)
+            ineq_norm = jnp.linalg.norm(jnp.clip(self.h_fn(x), a_min=0.), 1)
+            return self.f_fn(x) + sigma * (eq_norm + ineq_norm)
+        def merit_direc_deriv_fn(x, direction, sigma):
+            direct_deriv = jax.grad(self.f_fn)(x) @ direction
+            eq_1norm = jnp.linalg.norm(self.g_fn(x), 1)
+            ineq_1norm = jnp.linalg.norm(jnp.clip(self.h_fn(x),a_min=0.), 1)
+            return direct_deriv - sigma * (eq_1norm + ineq_1norm)
+        def armijo_cond_fn(x, direction, alpha, gamma, sigma):
+            curr_merit = merit_fn(x, sigma)
+            next_merit = merit_fn(x+alpha*direction, sigma)
+            merit_direc_deriv = merit_direc_deriv_fn(x, direction, sigma)
+            armijo = gamma * alpha * merit_direc_deriv
+            return next_merit < curr_merit + armijo
+        self.armijo_cond_fn = armijo_cond_fn
+        
+        if mode == "exact":
+            self.eval_fn = eval_fn_exact
+            self.state_init_fn = state_init_fn_exact
+        elif mode == "BFGS":
+            self.eval_fn = eval_fn_BFGS
+            self.state_init_fn = state_init_fn_BFGS
+
+        # precompile    
+        for att in dir(self):
+            if "fn" in att:
+                fn = jax.jit(getattr(self, att))
+                setattr(self, att, fn)
     
     def backtrack(
         self, 
@@ -84,13 +190,26 @@ class SQPSolver:
             alpha *= beta
         return alpha
     
-    def convexify(self, state):
+    def convexify(self, state:OptState):
         P = matrix(np.asarray(state.lag_hess).astype(np.double))
         q = matrix(np.asarray(state.f_grad).astype(np.double))
-        G = sparse(matrix(np.asarray(state.h_jac).astype(np.double)))
         h = matrix(-np.asarray(state.h_val).astype(np.double))
-        A = sparse(matrix(np.asarray(state.g_jac).astype(np.double)))
         b = matrix(-np.asarray(state.g_val).astype(np.double))
+        if self.sparse:
+            A = spmatrix(
+                np.asarray(state.g_jac, dtype=np.double), 
+                np.asarray(self.g_jac_sparsity_rowcol[0], dtype=int),
+                np.asarray(self.g_jac_sparsity_rowcol[1], dtype=int),
+            )
+            G = spmatrix(
+                np.asarray(state.h_jac, dtype=np.double), 
+                np.asarray(self.h_jac_sparsity_rowcol[0], dtype=int),
+                np.asarray(self.h_jac_sparsity_rowcol[1], dtype=int),
+            )
+        else:
+            G = sparse(matrix(np.asarray(state.h_jac).astype(np.double)))
+            A = sparse(matrix(np.asarray(state.g_jac).astype(np.double)))
+        
         return P, q, G, h, A, b
     
     def max_viol(self, state):
@@ -120,15 +239,16 @@ class SQPSolver:
         x = x0.copy()
         m = jnp.zeros(self.const_dim)
         sigma = 0.1
-        state = self.eval_fn(x, m, sigma)
+        xdiff = jnp.inf #initial value
+        state = self.state_init_fn(x, m, sigma)
 
         for i in range(max_iter):
             # Check terminal condition
-            if self.max_grad(state) < tol and \
+            if xdiff < tol and \
                self.max_viol(state) < const_viol_tol:
                 break
             elif save_history:
-                self.history.append(state)
+                self.states.append(state)
 
             # Solve QP
             P, q, G, h, A, b = self.convexify(state)
@@ -144,48 +264,74 @@ class SQPSolver:
             
             # linesearch and update
             alpha = self.backtrack(state, direction)
+            state_prev = state
             x, m, sigma = self.update(state, alpha, direction, dual_sol)
-            state = self.eval_fn(x, m, sigma)
+            xdiff = jnp.linalg.norm(direction)
+            state = self.eval_fn(x, m, sigma, state_prev)
             if verbose:
                 print(f"{i}: grad:{self.max_grad(state):.4f} | viol:{self.max_viol(state):.4f} | alpha:{alpha:.4f}")
+            if save_history:
+                self.history.append(History(alpha, direction))
         print("SQP solved !")
         return state.x
     
     def update(self, state:OptState, alpha, direction, dual_sol):
         x = state.x + alpha * direction
-        m = state.m * (1-alpha) + alpha * dual_sol
+        m = (1-alpha) + alpha * dual_sol
         sigma = jnp.linalg.norm(jnp.hstack([1.01*dual_sol, state.sigma]), jnp.inf)
         return x, m, sigma
     
     @classmethod
-    def from_builder(cls, nlp:NLPBuilder):
-        lag_fn = nlp.get_lagrangian_fn()
-        f_fn = nlp.f
-        g_fn = nlp.get_g_fn()
-        h_fn = nlp.get_h_fn()
-        merit_fn = nlp.get_merit_fn()
-        merit_direc_deriv_fn = nlp.get_merit_direc_deriv_fn()
-        @jax.jit
-        def eval_fn(x, m, sigma):
-            lag_grad = jax.grad(lag_fn)(x, m)
-            lag_hess = jax.hessian(lag_fn)(x, m)
-            lag_hess = regularized_hess(lag_hess)
-            f_val, f_grad = jax.value_and_grad(f_fn)(x)
-            g_val, g_jac = value_and_jacrev(x, f=g_fn)
-            h_val, h_jac = value_and_jacrev(x, f=h_fn)
-            return OptState(x, m, sigma, lag_grad, lag_hess, f_val, f_grad,
-                            g_val, g_jac, h_val, h_jac)
-        @jax.jit
-        def armijo_cond_fn(x, direction, alpha, gamma, sigma):
-            curr_merit = merit_fn(x, sigma)
-            next_merit = merit_fn(x+alpha*direction, sigma)
-            merit_direc_deriv = merit_direc_deriv_fn(x, direction, sigma)
-            armijo = gamma * alpha * merit_direc_deriv
-            return next_merit < curr_merit + armijo
-        return cls(
-            nlp.dim, nlp.const_dim,
-            eval_fn, armijo_cond_fn
-        )
+    def from_builder(cls, nlp:NLPBuilder, sparsity=True):
+        if sparsity:
+            return cls(
+                nlp.dim, nlp.g_dim, nlp.h_dim,
+                nlp.f, nlp.get_g_fn(), nlp.get_h_fn(),
+                nlp.get_g_jac_sparsity(),
+                nlp.get_h_jac_sparsity()
+            )
+        else:
+            return cls(
+                nlp.dim, nlp.g_dim, nlp.h_dim,
+                nlp.f, nlp.get_g_fn(), nlp.get_h_fn()
+            )
+        # lag_fn = nlp.get_lagrangian_fn()
+        # lag_grad_fn = jax.grad(lag_fn)
+        # lag_hess_fn = lambda x, m: regularized_hess(jax.hessian(lag_fn)(x, m))
+        # f_fn = nlp.f
+        # g_fn = nlp.get_g_fn()
+        # h_fn = nlp.get_h_fn()
+        # f_value_and_grad_fn = jax.value_and_grad(f_fn)
+        # g_value_and_jacrev_fn = partial(value_and_jacrev, f=g_fn)
+        # h_value_and_jacrev_fn = partial(value_and_jacrev, f=h_fn)
+        # merit_fn = nlp.get_merit_fn()
+        # merit_direc_deriv_fn = nlp.get_merit_direc_deriv_fn()
+        
+        # def eval_fn_exact_hess(x, m, sigma, state_prev):
+        #     lag_grad = lag_grad_fn(x, m)
+        #     lag_hess = lag_hess_fn(x, m)
+        #     f_val, f_grad = f_value_and_grad_fn(x)
+        #     g_val, g_jac = g_value_and_jacrev_fn(x)
+        #     h_val, h_jac = h_value_and_jacrev_fn(x)
+        #     return OptState(x, m, sigma, lag_grad, lag_hess, f_val, f_grad,
+        #                     g_val, g_jac, h_val, h_jac)
+        
+        # def eval_fn_BFGS(x, m, sigma, state_prev):
+        #     lag_grad = lag_grad_fn(x, m)
+        #     lag_hess = lag_hess_fn(x, m)
+        #     f_val, f_grad = f_value_and_grad_fn(x)
+        #     g_val, g_jac = g_value_and_jacrev_fn(x)
+        #     h_val, h_jac = h_value_and_jacrev_fn(x)
+        #     return OptState(x, m, sigma, lag_grad, lag_hess, f_val, f_grad,
+        #                     g_val, g_jac, h_val, h_jac)
+        
+        # def armijo_cond_fn(x, direction, alpha, gamma, sigma):
+        #     curr_merit = merit_fn(x, sigma)
+        #     next_merit = merit_fn(x+alpha*direction, sigma)
+        #     merit_direc_deriv = merit_direc_deriv_fn(x, direction, sigma)
+        #     armijo = gamma * alpha * merit_direc_deriv
+        #     return next_merit < curr_merit + armijo
+        
         
 
     # def solve(self, x0, lb, ub, cl, cu, opt:Dict):
